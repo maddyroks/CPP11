f7ef92d26c31abd346c89dd03b8552ff9c20211a
maddyroks <31966738+maddyroks@users.noreply.github.com>
2017-09-14 22:34:28 +0530

    First program

---
+++ Test.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : Test.cpp
+// Author      : d
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+int main(void) {
+	cout<<"Hi"<<endl;
+	int* p = nullptr;
+	    std::cout << p << std::endl;
+	    auto number = 1;
+	        cout << number;
+	return EXIT_SUCCESS;
+
+}
